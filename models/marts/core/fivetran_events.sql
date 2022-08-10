871382220494db47d050b48ef784c762fd2fb582
triggoa@gmail.com <triggoa@gmail.com>
2022-08-10 21:12:31 +0000

    fivetran-dbt-test

---
+++ models/marts/core/fivetran_events.sql
@@ -0,0 +1,16 @@
+with traffic_events as (
+    select * from {{source ('fivetran_sync', 'traffic')}}
+),
+
+weather_events as (
+    select city as city_2, type as weather_event, start_time_utc_ as weather_start, end_time_utc_ as weather_end  from {{source ('fivetran_sync', 'weather')}}
+),
+
+final as(
+select *
+    from traffic_events
+    left join weather_events
+    on traffic_events.city=weather_events.city_2 and (weather_events.weather_start <= traffic_events.start_time_utc_ and weather_events.weather_end >= traffic_events.start_time_utc_ )
+)
+
+select * from final
