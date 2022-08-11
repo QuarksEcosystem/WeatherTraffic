9181eb5924b43241ec0bc58a1ff4e88a257a3398
triggoa@gmail.com <triggoa@gmail.com>
2022-08-11 14:16:59 +0000

    deployment yml

--- models/marts/core/fivetran_events.sql
+++ models/marts/core/fivetran_events.sql
@@ -1,9 +1,9 @@
 with traffic_events as (
-    select * from {{source ('fivetran_sync', 'traffic')}}
+    select * from {{source ('traffic_weather_prod.traffic', 'traffic')}}
 ),
 
 weather_events as (
-    select city as city_2, type as weather_event, start_time_utc_ as weather_start, end_time_utc_ as weather_end  from {{source ('fivetran_sync', 'weather')}}
+    select city as city_2, type as weather_event, start_time_utc_ as weather_start, end_time_utc_ as weather_end  from {{source ('traffic_weather_prod.weather', 'weather')}}
 ),
 
 final as(
