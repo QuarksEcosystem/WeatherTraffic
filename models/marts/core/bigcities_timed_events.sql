4d5c1642eb16b59cc55d4f68aeefe140a5c4ca06
triggoa@gmail.com <triggoa@gmail.com>
2022-08-29 20:26:34 +0000

    n_events added to bigcities_timed

--- models/marts/core/bigcities_timed_events.sql
+++ models/marts/core/bigcities_timed_events.sql
@@ -3,7 +3,7 @@ with big_cities_days as (
 ),
 
 big_cities_hours_traffic as(
-    select big_cities_days.city,  to_timestamp(concat(days,' ', hours), 'dd/mm/yyyy hh24:mi:ss') as event_timestamp, listagg(distinct type, ', ') within group (order by type) as traffic_events from big_cities_days
+    select big_cities_days.city,  to_timestamp(concat(days,' ', hours), 'dd/mm/yyyy hh24:mi:ss') as event_timestamp, listagg(distinct type, ', ') within group (order by type) as traffic_events, count(type) as n_events from big_cities_days
     left join {{ref('stg_traffic_parsed_timezone')}}
     ON big_cities_days.city = {{ref('stg_traffic_parsed_timezone')}}.city and 
     {{ref('stg_traffic_parsed_timezone')}}.StartTime < to_timestamp(concat(days,' ', hours), 'dd/mm/yyyy hh24:mi:ss')  and 
@@ -12,7 +12,7 @@ big_cities_hours_traffic as(
 ),
 
 big_cities_hours_events as(
-    select big_cities_hours_traffic.city, event_timestamp, traffic_events, type as weather_event from big_cities_hours_traffic
+    select big_cities_hours_traffic.city, event_timestamp, traffic_events, n_events, type as weather_event from big_cities_hours_traffic
     left join {{ref('stg_weather_parsed_timezone')}}
     ON big_cities_hours_traffic.city = {{ref('stg_weather_parsed_timezone')}}.city and
     {{ref('stg_weather_parsed_timezone')}}.StartTime < event_timestamp  and 
