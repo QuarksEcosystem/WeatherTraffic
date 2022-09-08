40f3ae1e26ed44740e9e90313d94ddcf45ef61e7
triggoa@gmail.com <triggoa@gmail.com>
2022-09-08 13:38:49 +0000

    Big_Cities

--- models/marts/core/bigcities_timed_events.sql
+++ models/marts/core/bigcities_timed_events.sql
@@ -1,18 +1,17 @@
 with big_cities_days as (
-    select * from {{ref('big_cities_hours')}}
+    select * from {{ref('big_cities_hours')}} limit 10
 ),
 
 big_cities_hours_traffic as(
-    select big_cities_days.city,  to_timestamp(concat(days,' ', hours), 'dd/mm/yyyy hh24:mi:ss') as event_timestamp, listagg(distinct type, ', ') within group (order by type) as traffic_events, count(type) as n_events from big_cities_days
+    select big_cities_days.city,  to_timestamp(concat(days,' ', hours), 'dd/mm/yyyy hh24:mi:ss') as event_timestamp, type as traffic_events from big_cities_days
     left join {{ref('stg_traffic_parsed_timezone')}}
     ON big_cities_days.city = {{ref('stg_traffic_parsed_timezone')}}.city and 
     {{ref('stg_traffic_parsed_timezone')}}.StartTime < to_timestamp(concat(days,' ', hours), 'dd/mm/yyyy hh24:mi:ss')  and 
     {{ref('stg_traffic_parsed_timezone')}}.EndTime > to_timestamp(concat(days,' ', hours), 'dd/mm/yyyy hh24:mi:ss') 
-    group by big_cities_days.city, event_timestamp
 ),
 
 big_cities_hours_events as(
-    select big_cities_hours_traffic.city, event_timestamp, traffic_events, n_events, type as weather_event from big_cities_hours_traffic
+    select big_cities_hours_traffic.city, event_timestamp, traffic_events, type as weather_event from big_cities_hours_traffic
     left join {{ref('stg_weather_parsed_timezone')}}
     ON big_cities_hours_traffic.city = {{ref('stg_weather_parsed_timezone')}}.city and
     {{ref('stg_weather_parsed_timezone')}}.StartTime < event_timestamp  and 
--- models/staging/stg_houston_streets_timestamps.sql
+++ models/staging/stg_houston_streets_timestamps.sql
@@ -1,5 +1,5 @@
 with houston_streets as(
-    select city, street, sum(datediff(minute, START_TIME_UTC_, END_TIME_UTC_)) as total_time , TIME_ZONE from {{source ('traffic_events', 'US_TRAFFIC_EVENTS_RAW')}} where city = 'Houston' group by city, street, TIME_ZONE order by total_time desc limit 100
+    select city, street, sum(datediff(minute, START_TIME_UTC_, END_TIME_UTC_)) as total_time , TIME_ZONE from {{source ('traffic_events', 'US_TRAFFIC_EVENTS_RAW')}} where city = 'Houston' and state = 'TX' group by city, street, TIME_ZONE order by total_time desc limit 100
 ),
 houston_days as(
     select * 
